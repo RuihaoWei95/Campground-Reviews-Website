af37a33448527f62e5235329f433c11d5d9f511d
RuihaoWei95 <ruihw95@gmail.com>
2024-02-18 17:45:31 -0800

    Add flash

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const express = require('express');
-const path = require('path'); 
-const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
-const { campgroundSchema, reviewSchema } = require('./schemas.js');
-const joi = require('joi');
-const catchAsync = require('./utils/catchAsync');
-const methodOverride = require('method-override');
-const Campground = require('./models/campground');
-const ExpressError = require('./utils/ExpressError');
-const Review = require('./models/review');
-const campgrounds = require('./routes/campgrounds');
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const ejsMate = require('ejs-mate')
+const session = require('express-session')
+const flash = require('connect-flash')
+const { campgroundSchema, reviewSchema } = require('./schemas.js')
+const joi = require('joi')
+const catchAsync = require('./utils/catchAsync')
+const methodOverride = require('method-override')
+const Campground = require('./models/campground')
+const ExpressError = require('./utils/ExpressError')
+const Review = require('./models/review')
+const campgrounds = require('./routes/campgrounds')
 const reviews = require('./routes/reviews')
-mongoose.connect('mongodb://127.0.0.1:27017/campground');
+mongoose.connect('mongodb://127.0.0.1:27017/campground')
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -19,18 +21,38 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
-const app = express()
+const app = express();
 
-app.engine('ejs', ejsMate)
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname,'public')));
 
-// Router
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+const sessionConfig = {
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+};
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+app.use('/campgrounds', campgrounds);
+app.use('/campgrounds/:id/reviews', reviews);
 
 
 app.get('/', (req, res) => {
