4669090ca57cabb24006b6422174e2f8eb0b410c
RuihaoWei95 <ruihw95@gmail.com>
2024-02-26 18:24:50 -0800

    add image edit and delete

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,20 +1,23 @@
 const express = require('express');
+const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 const campgrounds = require('../controllers/campgrounds');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer = require('multer')
+const { storage } = require('../cloudinary');
+const upload = multer({storage});
 
-const router = express.Router();
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
+    
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
 router.route('/:id')
     .get(catchAsync(campgrounds.showCampground))
-    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
     .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.delete));
 
 
--- seeds/index.js
+++ seeds/index.js
@@ -24,7 +24,16 @@ const seedDB = async () => {
             author: `65d68307019fdf401b2bad4f`,
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: `https://source.unsplash.com/collection/483251`,
+            images: [
+                {   
+                url: 'https://res.cloudinary.com/dbyiarjq8/image/upload/v1708932496/YelpCamp/fhozfzcoo3rpdggv803o.jpg',
+                filename: "YelpCamp/qcprfuhgtmn6d1wqg0me"
+                },
+                {   
+                url: "https://res.cloudinary.com/dbyiarjq8/image/upload/v1708932496/YelpCamp/wgheakoegzwbpahs78i9.jpg",
+                filename: "YelpCamp/wgheakoegzwbpahs78i9",
+                }
+            ],
             price: price
         })
         await camp.save();
