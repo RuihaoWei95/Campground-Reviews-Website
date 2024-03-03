f781a026d88e1b5c8289016243b68b602acf2d79
RuihaoWei95 <ruihw95@gmail.com>
2024-03-03 00:57:18 -0800

    fix pop up by removing description because the seeded data doesn't have description

--- models/campground.js
+++ models/campground.js
@@ -38,12 +38,11 @@ const CampgroundSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'Review'
     }]
-}, opts)
+}, opts);
 
 CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
     return `
-    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
-    <p>${this.description.substring(0, 20)}...</p>`
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>`
 });
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
